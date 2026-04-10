31f3fc059f2c35145a3c4c93efe061feb3acac35
robertomduranf <robertomduranf@gmail.com>
2026-04-10 15:43:55 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Maneja caché offline + notificaciones de cobro
 // ============================================================
 
-var CACHE_NAME = 'aca-v39.5';
+var CACHE_NAME = 'aca-v40';
 var ASSETS = [
   './',
   './AlfajoresConAmor_Control.html',
