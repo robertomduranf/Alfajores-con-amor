190c663b6e2613882de9a23c9d2d7915f4845363
robertomduranf <robertomduranf@gmail.com>
2026-09-11 15:55:59 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // Cache: 1.3.13
 // ============================================================
 
-var CACHE_NAME = 'aca-V.a 1.3.14';
+var CACHE_NAME = 'aca-V.a 1.3.15';
 
 var URLS_TO_CACHE = [
   './',
